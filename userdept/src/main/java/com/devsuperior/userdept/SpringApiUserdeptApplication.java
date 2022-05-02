f674f892a0e59cd2313258471c0b7cf8c16c386b
Evelyn Alves <49756340+EvelynAlves1998@users.noreply.github.com>
2022-05-02 12:15:01 -0300

    Projeto Completo

---
+++ userdept/src/main/java/com/devsuperior/userdept/SpringApiUserdeptApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiUserdeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiUserdeptApplication.class, args);
+	}
+
+}
